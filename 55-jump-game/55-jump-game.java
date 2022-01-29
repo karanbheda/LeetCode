20bef262d35e4b158f2b5a4761e172e828a65baa
Karan Bheda <bhedakaran1@gmail.com>
2022-01-29 18:27:48 -0500

    Time: 868 ms (7.45%), Space: 72.6 MB (5.02%) - LeetHub

--- 55-jump-game/55-jump-game.java
+++ 55-jump-game/55-jump-game.java
@@ -1,15 +1,15 @@
 class Solution {
     public boolean canJump(int[] nums) {
         if(nums[0] == 0 && nums.length > 1) {return false;}
-        int[] dp = new int[nums.length];
-        Arrays.fill(dp, 1000001);
+        long[] dp = new long[nums.length];
+        Arrays.fill(dp, Integer.MAX_VALUE);
         dp[0] = 0;
         for(int i = 0; i < nums.length - 1; i++) {
             for(int j = 1; j <= nums[i] && i+j < nums.length; j++) {
                 dp[i+j] = Math.min(dp[i+j], dp[i] + 1);
             }
         }
-        System.out.print(Arrays.toString(dp));
-        return dp[nums.length - 1] < 1000001;
+        //System.out.print(Arrays.toString(dp));
+        return dp[nums.length - 1] < Integer.MAX_VALUE;
     }
 }
