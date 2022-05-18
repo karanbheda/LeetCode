cdef5b5d2d28d935d83c5b4b7eb31bf7907488be
Karan Bheda <bhedakaran1@gmail.com>
2022-05-18 08:44:15 -0400

    Time: 984 ms (92.17%), Space: 0B (100.00%) - LeetHub

---
+++ 1873-calculate-special-bonus/1873-calculate-special-bonus.sql
@@ -0,0 +1,4 @@
+/* Write your T-SQL query statement below */
+select employee_id, case when employee_id % 2 = 1 and name not like 'm%' then salary else 0 end bonus
+from Employees
+order by employee_id
